d9317949e105fba0e9dc0fe72b3825457594b632
KoshinKing <aking40@cherrycreekschools.org>
2024-09-03 19:05:01 +0000

    mmm

--- Sum.java
+++ Sum.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Sum{
     public static void main(String[] args){
+        Scanner word=new Scanner(System.in);
         int[]nums = {1,3,5,6,8,9,11,24,100};
-        System.out.println(sumChecker(nums,100));
+        System.out.println("Enter the number target: ");
+        System.out.println(sumChecker(nums,word.nextInt()));
     }
     public static String sumChecker(int[] nums,int targ)   {
         for(int i=0;i<nums.length-1;i++){
